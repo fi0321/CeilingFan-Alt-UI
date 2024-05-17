6468ddadaa21f05e39eaa601246ddf25e5a1ee06
fi0321 <fi0321@mun.ca>
2024-05-17 00:38:06 -0300

    changed present date

--- src/CeilingFan.java
+++ src/CeilingFan.java
@@ -3,17 +3,14 @@ import java.time.LocalDate;
 public class CeilingFan {
 	private int speed;// variable that holds the current speed of the fan
 	private boolean clockwiseDirection;// direction of the fan
-	private final LocalDate presentDate;// date of present day
 
 	public CeilingFan() {
 		this.speed = 0;
 		this.clockwiseDirection = true;
-		this.presentDate = LocalDate.now();
 
 		// Information the user that the fan will not operate on December 25.
 		if (isOffDay()) {
-			System.out
-					.println("The fan is off for today, pulling either of the cords will have no effect.");
+			System.out.println("The fan is off for today, pulling either of the cords will have no effect.");
 
 		}
 	}
@@ -31,13 +28,11 @@ public class CeilingFan {
 		this.speed = speed;
 	}
 
-	public LocalDate getPresentDate() {
-		return presentDate;
-	}
 
 	// Setting offday as true if the current date is December 25.
 	public boolean isOffDay() {
-		return this.presentDate.getMonthValue() == 12 && this.presentDate.getDayOfMonth() == 25;
+		LocalDate presentDate = LocalDate.now();
+		return presentDate.getMonthValue() == 12 && presentDate.getDayOfMonth() == 25;
 	}
 
 	// Method that enacts the pulling the cord for speed
@@ -45,8 +40,8 @@ public class CeilingFan {
 		// initially checks if it is an OffDay, if it is, it displays a message and returns
 		if (isOffDay()) {
 			speed = 0;
-			System.out.println(
-					"The fan is off. Pulling this cord on the 25th of December will have no effect.");
+			System.out
+					.println("The fan is off. Pulling this cord on the 25th of December will have no effect.");
 			return;
 		}
 		// cycling through the different options of the speed cord
@@ -62,8 +57,8 @@ public class CeilingFan {
 	public void pullFanDirectionCord() {
 		// initially checks if it is an OffDay, if it is, it displays a message and returns
 		if (isOffDay()) {
-			System.out.println(
-					"The fan is off. Pulling this cord on the 25th of December will have no effect.");
+			System.out
+					.println("The fan is off. Pulling this cord on the 25th of December will have no effect.");
 			return;
 		}
 		// reverses the direction of the fan
